429efb083a4a9f8a92ee37fb898fdb31486f800a
pavani <pavaniavni456@gmail.com>
2022-02-09 15:09:24 +0530

    added hashtable

---
+++ HashTables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+namespace HashTables { 
+    public class HashTable1
+    {
+        public static void Main(string[] args)
+        {
+
+            Hashtable h = new Hashtable();
+            int t=int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                h.Add(Console.ReadLine(), Console.ReadLine());
+            }
+            foreach (DictionaryEntry i in h)
+            {
+                Console.WriteLine(i.Key+" - "+i.Value);
+               
+            }
+        }
+
+
+    }
+}
